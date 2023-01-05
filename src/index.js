570935a0acd0af65ba0bff3e15ac13949c22b16f
Jeremy Hochstrasser <hochstrasserjeremy@gmail.com>
2023-01-04 17:41:23 -0700

    Implements Functionality and Working Result

--- src/index.js
+++ src/index.js
@@ -4,25 +4,49 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
+var fs = require('fs');
 
-
-
+var clientName = "name"
+var clientNotes = "mention notes"
+const Author = (process.env.AUTHOR);
 
 
 // Send call to GPT using prompt
-async function main() {
+async function main(Cli_Name,Cli_Note) {
     const response = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: "Say this is a test",
-        max_tokens: 25,
-        temperature: 0,
-      });
+        prompt: "Write a thank you letter from " + Author + " to " + Cli_Name + " that mentions the " + Cli_Note,
+        max_tokens: 200,
+        temperature: 0.3,
+      });``
 
     const completion = response.data.choices[0].text;
 
     console.log(completion);
 }
+// function to split file.txt on newlines -- seperated by a comma for each line
+function processFileLines(filePath, callback) {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) throw err;
+      const lines = data.split('\n');
+      lines.forEach(callback);
+    });
+  }
 //
 
+// read in the file.txt and split it and store it into var "array"
+function readAndSplit(line) {
+    var array = line.split(',');
+    clientName = array[0];
+    clientNotes = array[1];
+    
+    //console.log(clientName); 
+    //console.log(clientNotes);
 
-console.log(process.env.OPENAI_API_KEY);
+}
+
+processFileLines('file.txt', (line) => {
+    //console.log("\n__________________ New Entry")
+    readAndSplit(line);
+    main(clientName,clientNotes);
+  });
